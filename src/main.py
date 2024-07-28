65c95ba2b2eb3f18954c413d7b16aacc294d7d27
unknown <trinint3@gmail.com>
2024-07-27 20:49:06 -0400

    reworked logic flow

--- src/main.py
+++ src/main.py
@@ -1,138 +1,84 @@
-# webdriver
-from selenium import webdriver
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# standard
+import datetime
+from os import mkdir
+#import json
 
 # custom
 from video_data_class import video_data_class
 import youtube_dl_utils
+import web_driver_utils
 import log
 from user_modifcation import user_modification
-from user_video_choice import user_choice
+#from user_video_choice import user_choice
 
-# standard
-import datetime
-from os import mkdir
-import json
 
 
 def main():
+
+
+    # import config data
+    # download new playlist titles
+    # allow user to modify them
+    # search for video using using keyword searcher
+        # prompt to choose from results on zombie browser
+    # convert all choices to VDC
+    # download all songs
+    # hex all songs
+    # compile and store metadata
+
+
+
     start_time = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 
     #import config data
-    main_playlist = ""
+    #main_playlist = ""
     new_playlist = "https://www.youtube.com/playlist?list=PLqj_PfKdlkzdEfBQPW1dnnGc_whfcbxO0"
 
     #download song titles
     new_playlist_info = youtube_dl_utils.download_playlist_titles(new_playlist)
     new_playlist_name = new_playlist_info[0]
-    new_playlist_titles = new_playlist_info[1][:10]
-    
-    output_directory = f"./output/{new_playlist_name}_{start_time}"
-    mkdir(output_directory)
+    new_playlist_titles = new_playlist_info[1][:1] # unrefined playlist titles
 
+    #setup logging
     logger = log.create_logger(new_playlist_name)
 
-    #allow user modification
-    user_video_info = user_modification(new_playlist_titles, logger)
+
+    #create output directory
+    output_directory = f"./output/{new_playlist_name}_{start_time}"
+    mkdir(output_directory)
+    #create metadata file
+    metadata_file = f"{output_directory}/metadata.json"
+    with open(metadata_file, "w+", encoding="utf-8") as f:
+        f.close()
+
+
+    
+
+    #TODO check for duplicates and fill in cached titles and artists
+    # allow user modification
+    user_video_info = user_modification(new_playlist_titles, logger) # refined playlist titles
     logger.info("User modification complete")
 
-    #TODO check for duplicates
 
-    driver = webdriver.Firefox()
-    videos = []
-    # download page data, allow user to choose video, and convert it to VDC
+    video_objects = []
+    youtube_web_driver = web_driver_utils.youtube_web_driver()
     for info in user_video_info:
 
-        # TODO make this a module
-        # get the first 5 results for the given title and artist
-        video_title = info[0]
-        video_artist = info[1]
+        chosen_video_web_element = youtube_web_driver.find_official_song(info, logger)
+        new_vdc = video_data_class(chosen_video_web_element, info, output_directory)
 
-        base_search = "https://www.youtube.com/results?search_query="
-        search = f"{base_search}{video_title} - {video_artist}+%22topic%22"
-
-        driver.get(search)
-
-        content = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "contents"))
-        )
-        vids = content.find_elements(By.TAG_NAME, "ytd-video-renderer")[:10]
-
-        # convert choices to VDC
-        video_choices = []    
-        for vid in vids:
-            new_choice_vdc = video_data_class(vid, video_title, video_artist)
-
-            video_choices.append(new_choice_vdc)
-
-        # allow user to choose video from vdc choices and save it
-        new_vdc = user_choice(video_choices, logger, choices=5)
-        videos.append(new_vdc)
-
-    # cleanup
-    del(video_choices, content)
-    driver.close()
-
-
-    # setup metadata logging
-    with open(f"{output_directory}/metadata.json", "w+") as f:
-        f.close()
-    metadata = []
-
-
-    # download videos, hex file, log metadata
-    vdc: video_data_class
-    for i, vdc in enumerate(videos):
-        logger.info(f"Downloading {vdc.complete_title}")
-        res = youtube_dl_utils.bin_download_video(vdc, output_directory, logger)
-
-        if isinstance(res, str):
-            logger.info(f"{vdc.complete_title} downloaded successfully")
-            vdc.gen_hex()
-        elif isinstance(res, Exception):
-            logger.error(f"{vdc.complete_title} failed to download. Exception: {res}")
-            continue
-    
-        new_metadata = {
-            "title": vdc.user_video_title,
-            "artist": vdc.video_author,
-            "video_link": vdc.video_link,
-            "thumbnail_link": vdc.thumbnail_link,
-            "channel_name": vdc.channel_name,
-            "channel_link": vdc.channel_link,
-            "hex": vdc.file_hex
-        }
-
-        metadata.append(new_metadata)
-        
-        
-        """
-        with open(f"{output_directory}/metadata.json", "a+") as f:
-            if f.tell() == 0:
-                f.write("[")
-            elif f.tell() != 0 and i != len(vids) - 1:
-                f.write(",")
-            else:
-                json.dump(metadata, f, indent=4)
-                f.write("]")
-                continue
-            json.dump(metadata, f, indent=4)
-        """
-
-    with open(f"{output_directory}/metadata.json", "w+") as f:
-        json.dump(metadata, f, indent=4)
+        video_objects.append(new_vdc)
+    youtube_web_driver.close()
 
 
 
+    # download video_objects
+    youtube_dl_utils.download_all(video_objects, logger)
 
-
-
-    
-
-
-
+    # log metadata
+    video_object :video_data_class
+    for video_object in video_objects:
+        video_object.log_meta_data(output_directory)
 
 
 
