5b379276c56d5701d92b70d8cf5005138da2a9e0
trininite <trininit3@gmail.com>
2025-04-27 15:47:18 -0400

    fixed merge problems

--- src/json_utils/__init__.py
+++ src/json_utils/__init__.py
@@ -1,6 +1,6 @@
 # json_utils
 
-from .mirror_info import mirror_info_json_generator
+from .mirror_info import generate_mirror_info
 
 from .song_list import generate_song_list
 from .song_list import read_song_list
