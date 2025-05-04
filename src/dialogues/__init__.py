813a9b562a2f713bd7c13f50795a99ae0ba820b0
trininite <trininit3@gmail.com>
2025-05-04 15:06:01 -0400

    fixed import errors

--- src/dialogues/__init__.py
+++ src/dialogues/__init__.py
@@ -1,3 +1,3 @@
 
 from .startup import startup_dialogue
-from mirror_update import mirror_update_dialogue
+from .mirror_update import mirror_update_dialogue
--- src/mirror_utils/__init__.py
+++ src/mirror_utils/__init__.py
@@ -6,4 +6,4 @@ from .mirror_cache import create_mirror_cache
 
 from .duplicate_check import run_duplicate_check
 
-from update_names import run_name_updater
+from .update_names import run_name_updater
